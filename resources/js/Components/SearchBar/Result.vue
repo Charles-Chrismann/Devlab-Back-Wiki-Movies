610668642882ef32c37d0339b940bd58b9fc054c
Charles-Chrismann <charles.chrismann@free.fr>
2022-12-02 20:31:54 +0100

    :sparkles: ADD searchbar

--- resources/js/Components/SearchBar/Result.vue
+++ resources/js/Components/SearchBar/Result.vue
@@ -1,10 +1,14 @@
 <script setup>
-
+defineProps(['film'])
+// // console.log(film.value)
+// console.log("props.film.value")
+// console.log(film.value)
 </script>
 
 <template>
     <li>
-
+        <span>{{ film.original_title }}</span>
+        <img src="" alt="">
     </li>
 </template>
 
--- resources/js/Components/SearchBar/SearchBar.vue
+++ resources/js/Components/SearchBar/SearchBar.vue
@@ -2,16 +2,23 @@
 import axios from 'axios';
 
 import {ref} from "vue";
-import Result from "./Result";
+import Result from "./Result.vue";
 const searchbar = ref();
-// const ResultsList
-
+const results = ref([]);
+console.log(results.value)
 async function search(){
     console.log(searchbar.value.value)
+    if(!searchbar.value.value || searchbar.value.value === '') {
+        results.value = []
+        return
+    }
     axios.get(`/api/search/${searchbar.value.value}`)
         .then(function (response) {
             // handle success
             console.log(response.data);
+            results.value = response.data
+            console.log(results.value)
+            if(!searchbar.value.value || searchbar.value.value === '') results.value = []
         })
         .catch(function (error) {
             // handle error
@@ -27,7 +34,7 @@ async function search(){
 <template>
     <input type="text" @input="search" ref="searchbar">
     <ul>
-        <Result v-for="" />
+        <Result v-for="result in results" :film="result"/>
     </ul>
 </template>
 
