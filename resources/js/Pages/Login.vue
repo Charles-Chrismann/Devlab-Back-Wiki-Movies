caf224890772808d706892d7587b35d69c6b3e03
Tom Tamen <tomtamen@gmx.fr>
2022-12-07 00:36:35 +0100

    🚧 auth

--- resources/js/Pages/Login.vue
+++ resources/js/Pages/Login.vue
@@ -3,9 +3,9 @@
 </script>
 
 <template>
-    <form method="post">
-        <input type="email" placeholder="Email">
-        <input type="password" placeholder="Password">
+    <form v-csrf-token :action="route('connection')" method="post">
+        <input type="email" name="email" placeholder="Email">
+        <input type="password" name="password" placeholder="Password">
         <input type="submit" value="Register">
     </form>
 </template>
