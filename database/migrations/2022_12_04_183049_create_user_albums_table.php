79122fcdd0af32d510094fb914b219079f166f86
Tom Tamen <tomtamen@gmx.fr>
2022-12-27 22:12:09 +0100

    ✨ Create default albums when register

---
+++ database/migrations/2022_12_04_183049_create_user_albums_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_albums', function (Blueprint $table) {
+            $table->foreignId('user_id')
+                    ->constrained('users')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+
+            $table->foreignId('album_id')
+                    ->constrained('albums')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_albums');
+    }
+};
