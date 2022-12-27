5b46320ce4ef895b61582ad089780ccc9843c1fa
Charles-Chrismann <charles.chrismann@free.fr>
2022-12-27 23:23:28 +0100

    :construction: WIP UserProfile

--- resources/js/Pages/UserProfile.vue
+++ resources/js/Pages/UserProfile.vue
@@ -1,9 +1,16 @@
 <script setup>
 
 import { onMounted, ref, reactive } from 'vue';
+import { Link } from "@inertiajs/inertia-vue3";
 import Layout from '../Shared/Layout.vue';
 defineProps(['userDatas', 'albums', 'liked'])
 let isPersonal = ref(true);
+
+function showOptions(ref) {
+    console.log(document.querySelectorAll("[class*='options']"))
+    document.querySelectorAll("[class*='options']").forEach(item => {item.classList.remove("block"); item.classList.add("hidden");})
+    document.querySelector(ref).classList.remove("hidden")
+}
 </script>
 
 <template>
@@ -12,14 +19,18 @@ let isPersonal = ref(true);
 
         </div>
         <div class="albums overflow-hidden">
-            <div class="types w-full flex justify-between" :class="reactive({'isPersonal': !isPersonal})">
-                <p class="w-1/2 text-center" :class="reactive({'text-customGreen': isPersonal})" @click="isPersonal = true">Personal Albums</p>
-                <p class="w-1/2 text-center" :class="reactive({'text-customGreen': !isPersonal})" @click="isPersonal = false">Liked Albums</p>
+            <div class="types w-full flex justify-between relative cursor-pointer" :class="reactive({'isPersonal': !isPersonal})">
+                <p class="w-1/2 text-center py-4" :class="reactive({'text-customGreen': isPersonal, 'text-white': !isPersonal})" @click="isPersonal = true">Personal Albums</p>
+                <p class="w-1/2 text-center py-4" :class="reactive({'text-customGreen': !isPersonal, 'text-white': isPersonal})" @click="isPersonal = false">Liked Albums</p>
             </div>
-            <div class="albums__list flex w-full">
+            <div class="albums__list flex">
                 <div class="albums_personals w-full">
-                    <div v-for="album in albums">
-                        {{ album.name }}
+                    <div v-for="album, index in albums" class="w-full text-white flex justify-between px-8 py-4 relative">
+                        <span>Visibilté</span>
+                        <span>{{ album.name }}</span>
+                        <span class="z-10 cursor-pointer" @click="showOptions(`.optionsPersonal-${index}`)">. . .</span>
+                        <Link class="absolute inset-0" :href="'/username/albumName'">ffff</Link>
+                        <div :class="'optionsPersonal-' + index">ttt</div>
                     </div>
                 </div>
                 <div class="albums_liked w-full translate-x-full">
@@ -34,12 +45,27 @@ let isPersonal = ref(true);
 
 <style lang="scss" scoped>
 .albums {
+
+    .types::after {
+        content: '';
+        display: block;
+        width: 50%;
+        position: absolute;
+        height: 4px;
+        background-color: #42d392;
+        bottom: 0;
+        transition: all ease-in-out 0.5s;
+    }
     .types.isPersonal {
+        &::after {
+            transform: translateX(100%);
+        }
         & ~ .albums__list {
             transform: translateX(-100%);
         }
     }
     .albums__list {
+        width: 200%;
         transition: all ease-in-out 0.5s;
     }
 }
